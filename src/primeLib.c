28bf8c2134cb9faf2bfe1a62fbffec936e08c60a
Emmanuel Odeke <emmanuel.odeke@cmgcanada.ca>
2014-02-09 22:46:16 -0700

    More functions to primeLib

--- src/primeLib.c
+++ src/primeLib.c
@@ -65,6 +65,7 @@ HashMap *primes(uint64 primeCount) {
     ++i;
   }
 
+#ifdef PRINT_CONTENT
   if (__primeSav != NULL) {
     Chain **pIt = __primeSav->list, **pEnd = pIt + __primeSav->size;
     printf("%-20s%-10s\n", "Index", "Value");
@@ -78,8 +79,100 @@ HashMap *primes(uint64 primeCount) {
       ++pIt;
     }
   }
+#endif
 
   return __primeSav;
+}
+
+inline uint64 truncateLeft(uint64 n, const uint32 base) {
+  return base ? (n / base) : n;
+}
+
+inline uint64 truncateRight(uint64 n, const uint32 base) {
+  uint64 pow10 = log10(n);
+  uint64 mod = pow(base, pow10);
+  return n % mod;
+}
+
+Bool isFullyTruncatable(uint64 n, HashMap *primeH, uint64 (*truncator)(uint64, const uint32)) {
+  // Returns True if every truncated suffix of the integer n is also a prime
+  if (n < 11) return False;
+
+  Bool isFullyL = True;
+  Object *tmpO = intObject(n);
+  while (*(uint64 *)(tmpO->data) > 0) {
+    if (! __isPrime(tmpO, primeH)){
+      isFullyL = False;
+      break;
+    }
+
+    *((uint64 *)tmpO->data) = truncator(*((uint64 *)tmpO->data), 10);
+  }
+
+  tmpO = destroyObject(tmpO);
+
+  return isFullyL;
+}
+
+uint64 *truncatables(HashMap *primeH, uint64 (*f)(uint64, const uint32), uint32 *savSize) {
+  Chain **pIt = primeH->list, **pEnd = pIt + primeH->size;
+
+  uint32 i=0, bufSize = 100;
+  uint64 *sav = (uint64 *)malloc(sizeof(uint64) * bufSize);
+
+  while (pIt < pEnd) {
+    if (*pIt != NULL) {
+      Object *o = (*pIt)->value;
+      KeyValue *kv = o->data;
+      if (isFullyTruncatable(*(uint64 *)(kv->key->data), primeH, f) == True) {
+        if (i >= bufSize) {
+          bufSize += 100;
+          sav = (uint64 *)realloc(sav, sizeof(uint64) * bufSize);
+        }
+
+        *(sav + i) = *(uint64 *)(kv->key->data);
+        ++i;
+      }
+    }
+    ++pIt;
+  }
+
+  if (i) {
+    sav = (uint64 *)realloc(sav, sizeof(uint64) * i);
+  }
+
+  *savSize = i;
+  return sav;
+}
+
+int32 bSearch(const uint64 query, const uint64 *arr, const uint32 size) {
+  uint32 low=0, high=size-1, mid;
+  while (low <= high) {
+    mid = (low + high) >> 1;
+    if (arr[mid] == query) return mid;
+    if (arr[mid] < query) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+uint64 intersectionSum(const int64 *l, const uint32 lSize, const int64 *r, const uint32 rSize) {
+  uint64 sum = 0;
+  if (l != NULL && r != NULL) {
+    uint32 i;
+    for (i=0; i < lSize; ++i) {
+      if (bSearch(l[i], r, rSize) != -1) {
+        printf("Found: %ld\n", l[i]);
+        sum += l[i];
+      }
+    }
+  }
+
+  return sum;
 }
 
 int main(int argc, char *argv[]) {
@@ -91,6 +184,15 @@ int main(int argc, char *argv[]) {
   }
 
   HashMap *nprimes = primes(primeCount);
+
+  uint32 lSize, rSize; 
+  uint64 *lT = truncatables(nprimes, truncateLeft, &lSize);
+  uint64 *rT = truncatables(nprimes, truncateRight, &rSize);
+
+  printf("Sum:: %ld\n", intersectionSum(lT, lSize, rT, rSize));
+  if (lT != NULL) free(lT);
+  if (rT != NULL) free(rT);
+
   nprimes = destroyHashMap(nprimes);
   return 0;
 }
--- src/primeLib.h
+++ src/primeLib.h
@@ -6,5 +6,21 @@
   #include "hashMap.h"
 
   HashMap *primes(const uint64 primeCount);
+
   Bool __isPrime(const Object *numObject, HashMap *__primeSav);
+
+  int32 bSearch(const uint64 query, const uint64 *arr, const uint32 size);
+
+  uint64 intersectionSum(const int64 *l, const uint32 lSize, const int64 *r, const uint32 rSize);
+
+  // Returns True if a prime is truncatable and so are all it's
+  // [su/pre]fixes as determined by the truncator function
+  Bool isFullyTruncatable(uint64 n, HashMap *primeH, uint64 (*truncator)(uint64, const uint32));
+
+  // Given a truncator function, return a list of prime numbers that qualify
+  uint64 *truncatables(HashMap *primeH, uint64 (*f)(uint64, const uint32), uint32 *savSize);
+
+  // Truncator functions here
+  inline uint64 truncateLeft(uint64 n, const uint32 base);
+  inline uint64 truncateRight(uint64 n, const uint32 base);
 #endif
