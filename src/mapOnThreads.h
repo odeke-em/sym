d69edb0d23c4d1240d31c1bba7f3f26261f0aecd
Emmanuel Odeke <emmanuel.odeke@cmgcanada.ca>
2014-02-11 23:16:25 -0700

    Code for mapping using threads

---
+++ src/mapOnThreads.h
@@ -0,0 +1,22 @@
+#ifndef _MAP_ON_THREADS_H
+#define _MAP_ON_THREADS_H
+    typedef struct {
+        char **data;
+        unsigned int size;
+    } WordShell;
+
+    typedef struct {
+        char **wStart, **wEnd;
+        unsigned int (*filter)(const char *);
+    } WrapFilterShell;
+
+    WordShell *createWordShell(const int bufferSize);
+    WordShell *wordShellFromFile(const char *path, int (*customFilter)(const char));
+    WordShell *destroyWrap(WordShell *wp);
+    void printWrap(WordShell *wp);
+
+    // Prototype for function that will be passed as
+    // argument to thread to achieve filtration
+    void *filter(void *data);
+    WordShell *mapOnThreads(const char *path, unsigned int threadCount, unsigned int (*filterFunc)(const char *w));
+#endif
